8409cfa4cedda669d307c84939a840bdee5347b3
Pedro César <pedrocs378@gmail.com>
2021-08-02 01:16:26 -0300

    feat: adiciona funcao para criar agendamento

--- src/pages/CreateSchedule/index.tsx
+++ src/pages/CreateSchedule/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { ScrollView } from 'react-native'
-import { useRoute } from '@react-navigation/native'
+import { useNavigation, useRoute } from '@react-navigation/native'
 import { Calendar, LocaleConfig } from 'react-native-calendars'
 import { Ionicons } from '@expo/vector-icons'
 import { format } from 'date-fns'
@@ -13,6 +13,7 @@ import { api } from '../../services/api'
 import { colors } from '../../styles/colors'
 
 import * as S from './styles'
+import Toast from 'react-native-toast-message'
 
 LocaleConfig.locales['fr'] = {
 	monthNames: [
@@ -38,16 +39,51 @@ type RouteParams = {
 export function CreateSchedule() {
 	const [availableHours, setAvailableHours] = useState<AvailabilityData[]>([])
 	const [selectedHour, setSelectedHour] = useState(8)
-	const [selectedDate, setSelectedDate] = useState(new Date())
 	const [dateString, setDateString] = useState(() => {
 		return format(new Date(), 'yyyy-MM-dd')
 	})
+
+	const navigation = useNavigation()
 
 	const { params } = useRoute()
 	const { id: provider_id } = params as RouteParams
 
 	const handleChangeDate = (date: string) => {
 		setDateString(date)
+	}
+
+	const handleCreateSchedule = async () => {
+		try {
+			const [year, month, day] = dateString.split('-')
+
+			const date = new Date(Number(year), Number(month), Number(day), selectedHour)
+
+			await api.post('/appointments', {
+				provider_id,
+				date
+			})
+
+			Toast.show({
+				type: 'success',
+				position: 'bottom',
+				text1: 'Parabéns',
+				text2: 
+					`Você acabou de reservar um horário com este personal trainer. 
+					Para ver mais, vá no menu Listagem`
+			})
+	
+			navigation.reset({
+				index: 0,
+				routes: [{ name: 'AppTabs' }]
+			})
+		} catch {
+			Toast.show({
+				type: 'error',
+				position: 'bottom',
+				text1: 'Oops! Desculpe',
+				text2: `Aconteceu algo de errado. Tente novamente em instantes.`
+			})
+		}
 	}
 
 	const calendarArrow = (direction: 'left' | 'right') => {
@@ -63,20 +99,13 @@ export function CreateSchedule() {
 	}
 
 	useEffect(() => {
-		const currentDate = new Date(dateString)
-		currentDate.setHours(selectedDate.getHours())
-
-		setSelectedDate(currentDate)
-	}, [dateString])
-
-	useEffect(() => {
 		const [year, month, day] = dateString.split('-')
 
 		api
 			.get<AvailabilityData[]>(`/providers/${provider_id}/available_day_hours?year=${year}&month=${month}&day=${day}`)
 			.then(response => {
 				const hours = response.data
-					.filter(hour => hour.available)
+					.filter(hour => !!hour.available)
 					.map(hour => {
 						const currentHour = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), hour.hour)
 						
@@ -85,6 +114,7 @@ export function CreateSchedule() {
 							formatedHour: format(currentHour, "HH:mm'h'")
 						}
 					})
+				console.log(response.data)
 
 				setAvailableHours(hours)
 				setSelectedHour(hours[0].hour)
@@ -103,10 +133,11 @@ export function CreateSchedule() {
 						onDayPress={(date) => handleChangeDate(date.dateString)}
 						minDate={new Date()}
 						hideDayNames
+						hideExtraDays
 						enableSwipeMonths
 						monthFormat="MMMM"
+						
 						renderArrow={calendarArrow}
-						hideExtraDays
 						markedDates={{
 							[dateString]: {				
 								selected: true
@@ -151,7 +182,9 @@ export function CreateSchedule() {
 				</S.ScheduleContent>
 			</ScrollView>
 
-			<Button>Confirmar data</Button>
+			<Button onPress={handleCreateSchedule}>
+				Confirmar data
+			</Button>
 		</S.Container>
 	)
 }
